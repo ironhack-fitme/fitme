943b26d356144bec9c399356ea643af8d77436e6
jamil61 <jamisul6192@gmail.com>
2020-07-07 17:21:40 +0200

    profile 3rd attempt

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uploadCloud=require('../config/cloudinary')
 const {
   loginCheck
 } = require('./middlewares');
@@ -8,15 +9,38 @@ const User = require('../models/User')
 router.get('/user/:_id', loginCheck(), (req, res) => {
   const userId = req.user;
   console.log(userId)
-  User.findById(userId).then(userFromDataBase => {
-    console.log(userFromDataBase);
+  User.findById(userId).populate('activities').then(userFromDataBase => {
+    console.log(userFromDataBase.activites);
     res.render('user/profile', {
-      user: userFromDataBase
+      user: userFromDataBase,
     });
+    
   }).catch(err => {
     console.log(err);
   });
 });
+
+router.post("/profile/add", uploadCloud.single("photo"), (req, res, next) => {
+  const { title, description } = req.body;
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+User.findByIdAndUpdate(req.user._id,{avatar:imgPath}).then(userFromDB=>{
+res.redirect('/user/:_id')
+}) .catch(error => {
+  console.log(error);
+});
+  // Movie.create({ title, description, imgPath, imgName })
+  //   .then(movie => {
+  //     res.redirect("/");
+  //   })
+   
+});
+//---------------
+
+
+
+
+
 
 
 
@@ -30,3 +54,4 @@ router.get('/user/:_id', loginCheck(), (req, res) => {
 
 
 module.exports = router;
+// module.exports=uploader;
