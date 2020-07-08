bb0d2f589413ad0fc1b2455d990d5416f2445ad7
Majd Sabik <majdsabik.ms@gmail.com>
2020-07-08 20:27:34 +0200

    Further polished authjs file and removed unnecessary console.log commands

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,6 @@ router.get("/signup", (req, res) => {
 
 router.get("/logout", (req, res, next) => {
   // passport
-  console.log(req);
   req.logout();
   res.redirect("/");
 });
@@ -45,13 +44,11 @@ router.post(
   })
 );
 router.get("/logout", (req, res, next) => {
-  console.log(req);
   req.logout();
   res.redirect("/");
 });
 router.post("/signup", (req, res, next) => {
   const { username, password, email } = req.body;
-
   if (password.length < 8) {
     res.render("auth/signup", {
       message: "Your password must be 8 characters minimun.",
@@ -62,7 +59,6 @@ router.post("/signup", (req, res, next) => {
     res.render("auth/signup", { message: "Your username cannot be empty" });
     return;
   }
-
   User.findOne({ username: username }).then((found) => {
     if (found !== null) {
       res.render("auth/signup", { message: "Wrong credentials" });
@@ -88,45 +84,5 @@ router.post("/signup", (req, res, next) => {
     }
   });
 });
-
-/* router.post("/signup", (req, res, next) => {
-  const { username, password } = req.body;
-
-  if (password.length < 8) {
-    res.render("auth/signup", {
-      message: "Your password must be 8 characters minimun.",
-    });
-    return;
-  }
-  if (username === "") {
-    res.render("auth/signup", { message: "Your username cannot be empty" });
-    return;
-  }
-
-  User.findOne({ username: username }).then((found) => {
-    if (found !== null) {
-      res.render("auth/signup", { message: "Wrong credentials" });
-    } else {
-      // we can create a user with the username and password pair
-      const salt = bcrypt.genSaltSync();
-      const hash = bcrypt.hashSync(password, salt);
-      console.log(hash);
-      User.create({ username: username, password: hash })
-        .then((dbUser) => {
-          // passport - login the user
-          req.login(dbUser, (err) => {
-            if (err) next(err);
-            else res.redirect("/index/activities");
-          });
-
-          // redirect to login
-          res.redirect("login");
-        })
-        .catch((err) => {
-          next(err);
-        });
-    }
-  });
-}); */
 
 module.exports = router;
